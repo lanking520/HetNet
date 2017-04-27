39337053beb5bff6134ddb4c430834a288415880
Shen Zhu <shenzhu0127@gmail.com>
2017-04-27 14:42:38 -0400

    fix network latency bug

--- app/src/main/java/android_network/hetnet/networkcap/ConnectionEvalFetcher.java
+++ app/src/main/java/android_network/hetnet/networkcap/ConnectionEvalFetcher.java
@@ -13,6 +13,7 @@ import org.greenrobot.eventbus.EventBus;
 import java.io.DataInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.InetAddress;
 import java.net.URL;
 import java.net.URLConnection;
@@ -27,21 +28,24 @@ import static android_network.hetnet.common.Constants.CONNECTION_EVAL_FETCHER;
  */
 
 public class ConnectionEvalFetcher extends IntentService {
-    public ConnectionEvalFetcher(){super("ConnectionEvalFetcher");}
+    public ConnectionEvalFetcher() {
+        super("ConnectionEvalFetcher");
+    }
 
     private String bandwidth;
     private double latency;
     private String MAC_ADDR;
     private String sourceUrl = "http://download.thinkbroadband.com/10MB.zip";
+
     @Override
     public void onCreate() {
-        Log.i("CEL","Start Handele Connection Fetcher");
+        Log.i("CEL", "Start Handele Connection Fetcher");
         super.onCreate();
     }
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        Log.i("CEL","Start Handele Connection Fetcher");
+        Log.i("CEL", "Start Handele Connection Fetcher");
         NetworkEvaluation eval = new NetworkEvaluation();
         downLoadFileFromServer(sourceUrl);
         eval.setBandwidth(this.bandwidth);
@@ -59,31 +63,30 @@ public class ConnectionEvalFetcher extends IntentService {
     }
 
     private void downLoadFileFromServer(String sourceUrl) {
-        Log.v("DEBUG", "sourceUrl: "+sourceUrl);
+        Log.v("DEBUG", "sourceUrl: " + sourceUrl);
 
-        InputStream urlInputStream=null;
+        InputStream urlInputStream = null;
 
-        URLConnection urlConnection ;
-        try
-        {
+        URLConnection urlConnection;
+        try {
             long start = System.currentTimeMillis();
 
             //Form a new URL
-            URL finalUrl =new URL(sourceUrl);
+            URL finalUrl = new URL(sourceUrl);
 
             urlConnection = finalUrl.openConnection();
 
             //Get the size of the (file) inputstream from server..
-            int contentLength=urlConnection.getContentLength();
+            int contentLength = urlConnection.getContentLength();
 
-            Log.d("1URL","Streaming from "+sourceUrl+ "....");
+            Log.d("1URL", "Streaming from " + sourceUrl + "....");
             DataInputStream stream = new DataInputStream(finalUrl.openStream());
 
-            Log.d("2FILE","Buffering the received stream(size="+contentLength+") ...");
+            Log.d("2FILE", "Buffering the received stream(size=" + contentLength + ") ...");
             byte[] buffer = new byte[contentLength];
             stream.readFully(buffer);
             stream.close();
-            Log.d("3FILE","Buffered successfully(Buffer.length="+buffer.length+")....");
+            Log.d("3FILE", "Buffered successfully(Buffer.length=" + buffer.length + ")....");
 
             long end = System.currentTimeMillis();
 
@@ -91,23 +94,16 @@ public class ConnectionEvalFetcher extends IntentService {
 
             Log.i("SPDTST", "bandwidth = " + bandwidth + " Mbps");
             this.bandwidth = String.valueOf(bandwidth);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             Log.e("9ERROR", "Failed to open urlConnection/Stream the connection(From catch block) & returning 'false'..");
             System.out.println("Exception: " + e);
             e.printStackTrace();
-        }
-        finally
-        {
-            try
-            {
+        } finally {
+            try {
                 Log.d("10URL", "Closing urlInputStream... ");
                 if (urlInputStream != null) urlInputStream.close();
 
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 Log.e("11ERROR", "Failed to close urlInputStream(From finally block)..");
             }
         }
@@ -115,31 +111,31 @@ public class ConnectionEvalFetcher extends IntentService {
 
 
     private void testNetworkLatency() {
-        String host = "www.google.com";
-
-        long timeSum = 0;
-        long beforeTime;
-        long afterTime;
-
-        int timeOut = 8000;
-        int times = 3;
-
-        double networkLatency;
-
         try {
+            URL url = new URL("http://www.google.com");
+
+            long timeSum = 0;
+            long beforeTime;
+            long afterTime;
+
+            int times = 3;
+
+            double networkLatency;
+
             for (int i = 0; i < times; i++) {
                 beforeTime = System.currentTimeMillis();
-                boolean reachable = InetAddress.getByName(host).isReachable(timeOut);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                Object responseObject = urlConnection.getContent();
                 afterTime = System.currentTimeMillis();
 
                 timeSum += (afterTime - beforeTime);
             }
 
             networkLatency = timeSum / (times * 1.0);
-            Log.d("CEL",Double.toString(networkLatency));
+            Log.d("CEL", Double.toString(networkLatency));
             this.latency = networkLatency;
         } catch (Exception e) {
-            Log.d("CEL","ERROR: Some Error comes out from testNetwork Latency");
+            Log.d("CEL", "ERROR: Some Error comes out from testNetwork Latency");
             e.printStackTrace();
         }
     }
