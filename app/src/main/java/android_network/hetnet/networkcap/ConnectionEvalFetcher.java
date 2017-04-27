ba08909b64eec3a73b810a994c42eb1703b9cbb1
lanking520 <lanking520@gmail.com>
2017-04-27 13:16:52 -0400

    add kai';s function

--- app/src/main/java/android_network/hetnet/networkcap/ConnectionEvalFetcher.java
+++ app/src/main/java/android_network/hetnet/networkcap/ConnectionEvalFetcher.java
@@ -1,10 +1,17 @@
 package android_network.hetnet.networkcap;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 import org.greenrobot.eventbus.EventBus;
 
+import java.io.DataInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Calendar;
 
 import android_network.hetnet.data.NetworkEvaluation;
@@ -17,6 +24,9 @@ import static android_network.hetnet.common.Constants.CONNECTION_EVAL_FETCHER;
 
 public class ConnectionEvalFetcher extends IntentService {
     public ConnectionEvalFetcher(){super("ConnectionEvalFetcher");}
+
+    private String bandwidth;
+    private String sourceUrl = "http://download.thinkbroadband.com/10MB.zip";
     @Override
     public void onCreate() {
         super.onCreate();
@@ -25,7 +35,66 @@ public class ConnectionEvalFetcher extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         NetworkEvaluation eval = new NetworkEvaluation();
+        downLoadFileFromServer(sourceUrl);
+        eval.setBandwidth(this.bandwidth);
+        
         // TODO: Implement fetcher for things we need
         EventBus.getDefault().post(new ConnectionResponseEvent(CONNECTION_EVAL_FETCHER, eval, Calendar.getInstance().getTime()));
     }
+
+    private void downLoadFileFromServer(String sourceUrl) {
+        Log.v("DEBUG", "sourceUrl: "+sourceUrl);
+
+        InputStream urlInputStream=null;
+
+        URLConnection urlConnection ;
+        try
+        {
+            long start = System.currentTimeMillis();
+
+            //Form a new URL
+            URL finalUrl =new URL(sourceUrl);
+
+            urlConnection = finalUrl.openConnection();
+
+            //Get the size of the (file) inputstream from server..
+            int contentLength=urlConnection.getContentLength();
+
+            Log.d("1URL","Streaming from "+sourceUrl+ "....");
+            DataInputStream stream = new DataInputStream(finalUrl.openStream());
+
+            Log.d("2FILE","Buffering the received stream(size="+contentLength+") ...");
+            byte[] buffer = new byte[contentLength];
+            stream.readFully(buffer);
+            stream.close();
+            Log.d("3FILE","Buffered successfully(Buffer.length="+buffer.length+")....");
+
+            long end = System.currentTimeMillis();
+
+            double bandwidth = 10.0 * 8 * 1000 / (end - start);
+
+            Log.i("SPDTST", "bandwidth = " + bandwidth + " Mbps");
+            this.bandwidth = String.valueOf(bandwidth);
+        }
+        catch (Exception e)
+        {
+            Log.e("9ERROR", "Failed to open urlConnection/Stream the connection(From catch block) & returning 'false'..");
+            System.out.println("Exception: " + e);
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                Log.d("10URL", "Closing urlInputStream... ");
+                if (urlInputStream != null) urlInputStream.close();
+
+            }
+            catch (Exception e)
+            {
+                Log.e("11ERROR", "Failed to close urlInputStream(From finally block)..");
+            }
+        }
+    }
+
 }
